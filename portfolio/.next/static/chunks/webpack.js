1d36e2673ea9452f9d2618fda449a2d6553a6dca
Asad <hmasad09@gmail.com>
2020-11-29 20:20:15 +0800

    fixed link with no refresh page

--- portfolio/.next/static/chunks/webpack.js
+++ portfolio/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3176302ae86e462112a4";
+/******/ 	var hotCurrentHash = "575698ce9653c2616bf4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- portfolio/components/shared/Navbar.js
+++ portfolio/components/shared/Navbar.js
@@ -1,5 +1,5 @@
-
 import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
+import Link from 'next/link'
 
 
 const AppNavbar = () => {
@@ -7,27 +7,43 @@ const AppNavbar = () => {
   return (
     <div className="navbar-wrapper">
       <Navbar expand="lg" className="navbar-dark fj-mw9">
-        <Navbar.Brand className="mr-3 font-weight-bold" href="/">How-i-code</Navbar.Brand>
+        <Navbar.Brand className="mr-3 font-weight-bold">
+            <Link href="/" className="mr-3">
+              <a className="navbar-brand mr-3 font-weight-bold">
+                How-i-code
+              </a>
+            </Link>
+        </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav className="mr-auto">
-            <Nav.Link href="/portfolios" className="mr-3">
-              Portfolios
-            </Nav.Link>
-            <Nav.Link href="/forum/categories" className="mr-3">
-              Forum
-            </Nav.Link>
-            <Nav.Link href="#" className="mr-3">
-              Cv
-            </Nav.Link>
+            <Link href="/portfolios" className="mr-3">
+              <a className="mr-3 nav-link">
+                Portfolios
+              </a>
+            </Link>
+            <Link href="/forum/categories" className="mr-3">
+              <a className="mr-3 nav-link">
+                Forum
+              </a>
+            </Link>
+            <Link href="/cv" className="mr-3">
+              <a className="mr-3 nav-link">
+                CV
+              </a>
+            </Link>
           </Nav>
           <Nav>
-            <Nav.Link href="#" className="mr-3">
-              Sign Up
-            </Nav.Link>
-            <Nav.Link href="#" className="mr-3 btn btn-success bg-green-2 bright">
-              Sign In
-            </Nav.Link>
+          <Link href="/signup" className="mr-3">
+              <a className="mr-3 nav-link">
+                Signup
+              </a>
+            </Link>
+            <Link href="/signin" className="mr-3">
+              <a className="mr-3 nav-link">
+                Signin
+              </a>
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
